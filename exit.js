26de14d3a71590bf16e881469e4733579d6d3df0
elixcat <142026724+elixcat@users.noreply.github.com>
2026-08-23 15:44:29 +0300

    Fix reload button - use direct location.reload() without confirmation

--- exit.js
+++ exit.js
@@ -21,6 +21,10 @@
       location.reload();
     }
   }
+
+  function reloadApp() {
+    window.location.reload();
+  }
   
   function addListener(element, callback) {
     if (typeof $ !== 'undefined' && typeof $(element).on === 'function') {
@@ -42,7 +46,7 @@
     var reloadButtonHTML =
       '<div id="RELOAD" class="head__action selector reload-screen" tabindex="0">' +
         '<svg fill="#ffffff" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">' +
-          '<path d="M4,12a1,1,0,0,1-2,0A9.983,9.983,0,0,1,18.242,4.206V2.758a1,1,0,1,1,2,0v4a1,1,0,0,1-1,1h-4a1,1,0,0,1,0-2h1.743A7.986,7.986,0,0,0,4,12Zm17-1a1,1,0,0,0-1,1A7.986,7.986,0,0,1,7.015,18.242H8.757a1,1,0,1,0,0-2h-4a1,1,0,0,0-1,1v4a1,1,0,0,0,2,0V19.794A9.984,9.984,0,0,0,22,12,1,1,0,0,0,21,11Z" fill="currentColor"></path>' +
+          '<path d="M4,12a1,1,0,0,1-2,0A9.983,9.983,0,0,1,18.242,4.206V2.758a1,1,0,1,1,2,0v4a1,1,0,0,1-1,1h-4a1,1,0,0,1,0-2h1.743A7.986,7.986,0,0,0,4,12Zm17-1a1,1,0,0,0-1,1A7.986,7.986,0,0,1,7.015,18.[...]' +
         '</svg>' +
       '</div>';
 
@@ -58,7 +62,7 @@
     headerActions.insertAdjacentHTML('beforeend', reloadButtonHTML + exitButtonHTML);
 
     var reloadButton = document.getElementById('RELOAD');
-    if (reloadButton) addListener(reloadButton, function() { location.reload(); });
+    if (reloadButton) addListener(reloadButton, reloadApp);
 
     var exitButton = document.getElementById('EXIT');
     if (exitButton) addListener(exitButton, exitLamp);
