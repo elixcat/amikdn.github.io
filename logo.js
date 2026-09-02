7986178ceeb8bc211b1e0cfba212d5ffdd2de2df
elixcat <142026724+elixcat@users.noreply.github.com>
2026-09-02 05:05:11 +0300

    Update logo field descriptions for clarity

--- logo.js
+++ logo.js
@@ -10,7 +10,7 @@
     Lampa.SettingsApi.addParam({
         component: "interface",
         param: { name: "logo_size", type: "select", values: { w300: "w300", w500: "w500", w780: "w780", original: "Оригінал" }, default: "w500" },
-        field: { name: "Розмір логотипу", description: "Дозвіл завантажуваного зображення" }
+        field: { name: "Розмір логотипу", description: "Розмір зображення" }
     });
 
     Lampa.SettingsApi.addParam({
@@ -22,7 +22,7 @@
     Lampa.SettingsApi.addParam({
         component: "interface",
         param: { name: "logo_info_scale", type: "select", values: { "1": "100%", "1.1": "110%", "1.2": "120%", "1.3": "130%", "1.4": "140%", "1.5": "150%", "1.6": "160%", "1.7": "170%", "1.8": "180%", "1.9": "190%", "2": "200%", "2.1": "210%", "2.2": "220%", "2.3": "230%", "2.4": "240%", "2.5": "250%" }, default: "1" },
-        field: { name: "Масштаб інфо про фільм", description: "Змінює розмір шрифту інформації про рік та країну" }
+        field: { name: "Масштаб інформації про фільм", description: "Змінює розмір шрифту інформації про рік, країну, жанр та тривалість перегляду." }
     });
 
     if (window.logoplugin) return;
