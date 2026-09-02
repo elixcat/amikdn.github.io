56a5124004a76ef36ac3ed751d3a2d383633e5c3
elixcat <142026724+elixcat@users.noreply.github.com>
2026-09-02 05:52:58 +0300

    Refactor logo scaling logic and remove unused function
    Removed unused getScaleValues function and replaced its usage with hardcoded scale values for logo_info_scale. Updated the handling of logo display and scaling in the interface.

--- logotip.js
+++ logotip.js
@@ -11,21 +11,9 @@
             border-radius: 6px;
             margin-right: 8px;
             font-weight: bold;
-            display: inline-block;
-            vertical-align: middle;
         }
     `;
     document.head.appendChild(style);
-
-    // Функція для створення значень масштабу
-    function getScaleValues() {
-        var vals = {};
-        for (var i = 5; i <= 20; i++) {
-            var val = (i / 10).toString();
-            vals[val] = (i * 10) + "%";
-        }
-        return vals;
-    }
 
     Lampa.SettingsApi.addParam({
         component: "interface",
@@ -47,24 +35,20 @@
 
     Lampa.SettingsApi.addParam({
         component: "interface",
-        param: { name: "logo_info_scale", type: "select", values: getScaleValues(), default: "1" },
-        field: { name: "Масштаб інформації (рік, жанри)", description: "Масштаб для всього, крім часу." }
-    });
-
-    Lampa.SettingsApi.addParam({
-        component: "interface",
-        param: { name: "logo_time_scale", type: "select", values: getScaleValues(), default: "1" },
-        field: { name: "Масштаб часу перегляду", description: "Окремий масштаб тільки для часу." }
+        param: { name: "logo_info_scale", type: "select", values: { "1": "100%", "1.1": "110%", "1.2": "120%", "1.3": "130%", "1.4": "140%", "1.5": "150%", "1.6": "160%", "1.7": "170%", "1.8": "180%", "1.9": "190%", "2": "200%", "2.1": "210%", "2.2": "220%", "2.3": "230%", "2.4": "240%", "2.5": "250%" }, default: "1" },
+        field: { name: "Масштаб інформації про фільм", description: "Змінює розмір шрифту інформації про рік, країну, жанр та тривалість перегляду." }
     });
 
     if (window.logoplugin) return;
     window.logoplugin = true;
 
     Lampa.Storage.listener.follow('change', function (e) {
-        if (['logo_glav', 'logo_size', 'logo_hide_year', 'logo_info_scale', 'logo_time_scale'].includes(e.param)) {
+        if (['logo_glav', 'logo_size', 'logo_hide_year', 'logo_info_scale'].includes(e.param)) {
             var activity = Lampa.Activity.active();
             if (activity && activity.component === 'full') {
-                setTimeout(function () { activity.reload(); }, 300);
+                setTimeout(function () {
+                    activity.reload();
+                }, 300);
             }
         }
     });
@@ -74,6 +58,7 @@
             var movie = a.data.movie;
             var type = movie.name ? 'tv' : 'movie';
             var render = a.object.activity.render();
+
             var title = render.find(".full-start-new__title");
             var head = render.find(".full-start-new__head");
             var details = render.find(".full-start-new__details");
@@ -83,49 +68,58 @@
 
             var lang = Lampa.Storage.get("language");
             var size = Lampa.Storage.get("logo_size", "w500");
-            var infoScale = parseFloat(Lampa.Storage.get("logo_info_scale", "1"));
-            var timeScale = parseFloat(Lampa.Storage.get("logo_time_scale", "1"));
+            var infoScale = Lampa.Storage.get("logo_info_scale", "1");
 
             var TMDB_API = "http://apitmdb.cubnotrip.top/3";
             var url = TMDB_API + "/" + type + "/" + movie.id +"/images?api_key=" + Lampa.TMDB.key() +"&include_image_language=" + lang + ",en,null";
 
             $.get(url, function (response) {
-                // ... (код отримання логотипу залишається без змін)
                 var logo_path = null;
+
                 if (response.logos && response.logos.length > 0) {
-                    for (var i = 0; i < response.logos.length; i++) { if (response.logos[i].iso_639_1 == lang) { logo_path = response.logos[i].file_path; break; } }
-                    if (!logo_path) { for (var i = 0; i < response.logos.length; i++) { if (response.logos[i].iso_639_1 == 'en') { logo_path = response.logos[i].file_path; break; } } }
-                    if (!logo_path) { logo_path = response.logos[0].file_path; }
+                    for (var i = 0; i < response.logos.length; i++) {
+                        if (response.logos[i].iso_639_1 == lang) {
+                            logo_path = response.logos[i].file_path;
+                            break;
+                        }
+                    }
+                    if (!logo_path) {
+                        for (var i = 0; i < response.logos.length; i++) {
+                            if (response.logos[i].iso_639_1 == 'en') {
+                                logo_path = response.logos[i].file_path;
+                                break;
+                            }
+                        }
+                    }
+                    if (!logo_path) {
+                        logo_path = response.logos[0].file_path;
+                    }
                 }
 
                 if (logo_path) {
                     var logo_url = Lampa.TMDB.image("/t/p/" + (size === "original" ? "original" : size) + logo_path.replace(".svg", ".png"));
+
                     title.html('<img style="margin-top:5px; max-height:125px;" src="' + logo_url + '"/>');
+
                     tagline.remove();
 
                     if (Lampa.Storage.get("logo_hide_year", true)) {
                         if (head.length && details.length && details.find(".logo-moved-head").length === 0) {
                             var head_html = head.html().trim();
                             if (head_html) {
-                                var raw_details = details.html();
-                                var time_match = raw_details.match(/(\d{1,2}:\d{2})/);
-                                var time_html = '';
-                                
-                                // Вирізаємо час із загального тексту
-                                if (time_match) {
-                                    time_html = '<span class="time-badge" style="font-size:' + (timeScale * 100) + '%;">' + time_match[0] + '</span>';
-                                    raw_details = raw_details.replace(time_match[0], '');
-                                }
-
-                                // Масштабуємо решту контенту
+                                var details_html = details.html();
                                 var scalePercent = (infoScale * 100);
-                                var moved_head = '<div class="logo-moved-head" style="margin-left:0.6em; display:block; width:100%; clear:both; margin-bottom:0.5em; font-size:' + scalePercent + '%;">' + head_html + '</div>';
                                 
-                                var scaled_details = raw_details.replace(/(<span|<div)/g, function(match) {
+                                // Логіка обгортання часу в плашку
+                                // Знаходить формат часу (цифри:цифри) і додає клас
+                                details_html = details_html.replace(/(\d{1,2}:\d{2})/g, '<span class="time-badge">$1</span>');
+
+                                var moved_head = '<div class="logo-moved-head" style="margin-left:0.6em; display:block; width:100%; clear:both; margin-bottom:0.5em; font-size:' + scalePercent + '%;">' + head_html + '</div>';
+                                var scaled_details = details_html.replace(/(<span|<div)/g, function(match) {
                                     return match + ' style="font-size:' + scalePercent + '%;"';
                                 });
                                 
-                                details.html(moved_head + time_html + scaled_details);
+                                details.html(moved_head + scaled_details);
                                 head.remove();
                             }
                         }
